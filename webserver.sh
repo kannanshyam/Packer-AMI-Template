0f967428b37ff5e61e32f944e0c91390980396b6
Kannan MS <94454999+kannanshyam@users.noreply.github.com>
2021-12-07 06:47:19 +0530

    Create webserver.sh

---
+++ webserver.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo yum install httpd git -y
+
+sudo systemctl restart httpd.service
+sudo systemctl enable httpd.service
+
+cd /tmp/
+sudo git clone https://github.com/kannanshyam/blog-website.git
+sudo cp -pr sample-HTML-website/* /var/www/html
+sudo chown -R apache. /var/www/html/
+
+sudo systemctl restart httpd.service
