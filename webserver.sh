f8021b76875cc052bbf2cdd47c775cd16d010cb1
Kannan MS <94454999+kannanshyam@users.noreply.github.com>
2021-12-07 12:32:55 +0530

    Updated the content

--- webserver.sh
+++ webserver.sh
@@ -7,7 +7,7 @@ sudo systemctl enable httpd.service
 
 cd /tmp/
 sudo git clone https://github.com/kannanshyam/blog-website.git
-sudo cp -pr sample-HTML-website/* /var/www/html
+sudo cp -pr blog-website/* /var/www/html
 sudo chown -R apache. /var/www/html/
 
 sudo systemctl restart httpd.service
